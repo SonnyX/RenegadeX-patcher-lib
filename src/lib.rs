fd1636d1419770cd15795a1b53a96c66de09aeab
SonnyX <randy@vonderweide.nl>
2019-02-10 12:30:18 +0100

    Add traits to simplify programming, use structs in favor of JSON objects

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,52 @@ use std::io;
 use std::io::{Read, Write, Seek, SeekFrom};
 use std::fs::{File,OpenOptions,DirBuilder};
 
+trait AsString {
+  fn as_string(&self) -> Option<String>;
+}
+
+impl AsString for json::JsonValue {
+    fn as_string(&self) -> Option<String> {
+        match *self {
+            json::JsonValue::Short(ref value)  => Some(value.to_string()),
+            json::JsonValue::String(ref value) => Some(value.to_string()),
+            _                                  => None
+        }
+    }
+}
+
+trait BorrowUnwrap<T> {
+  fn borrow(&self) -> &T;
+}
+
+impl<T> BorrowUnwrap<T> for Option<T> {
+  fn borrow(&self) -> &T {
+    match self {
+      Some(val) => val,
+      None => panic!("called `Option::borrow()` on a `None` value"),
+    }
+  }
+}
+
+#[derive(Debug,Clone)]
+pub struct Instruction {
+  path: String,
+  old_hash: Option<String>,
+  new_hash: Option<String>,
+  compressed_hash: Option<String>,
+  delta_hash: Option<String>,
+  old_last_write_time: String,
+  new_last_write_time: String,
+  full_replace_size: usize,
+  delta_size: usize,
+  has_delta: bool
+}
+
 pub struct Downloader {
   RenegadeX_location: Option<String>, //Os dependant
   version: Option<String>, //RenegadeX version as mentioned in release.json
   release_json: Option<json::JsonValue>, //release.json
-  instructions_json: Option<json::JsonValue>, //instructions.json
+  instructions: Vec<Instruction>, //instructions.json
   compressed_size: Option<f64>, //summed download size from instructions.json
   instructions_hash: Option<String>, //Hash of instructions.json
 }
@@ -28,7 +69,7 @@ impl Downloader {
       RenegadeX_location: None,
       version: None,
       release_json: None,
-      instructions_json: None,
+      instructions: Vec::new(),
       compressed_size: None,
       instructions_hash: None,
     };
@@ -105,7 +146,24 @@ impl Downloader {
       Ok(result) => result,
       Err(e) => panic!("Invalid JSON: {}", e)
     };
-    self.instructions_json = Some(instructions_data.clone());
+
+    let mut InstructionArray : Vec<Instruction> = Vec::with_capacity(instructions_data.len());
+    for i in 0..instructions_data.len() {
+      InstructionArray.push(Instruction {
+        path: instructions_data[i]["Path"].as_string().unwrap(),
+        old_hash: instructions_data[i]["OldHash"].as_string(),
+        new_hash: instructions_data[i]["NewHash"].as_string(),
+        compressed_hash: instructions_data[i]["CompressedHash"].as_string(),
+        delta_hash: instructions_data[i]["DeltaHash"].as_string(),
+        old_last_write_time: instructions_data[i]["OldLastWriteTime"].as_string().unwrap(),
+        new_last_write_time: instructions_data[i]["NewLastWriteTime"].as_string().unwrap(),
+        full_replace_size: instructions_data[i]["FullReplaceSize"].as_usize().unwrap(),
+        delta_size: instructions_data[i]["DeltaSize"].as_usize().unwrap(),
+        has_delta: instructions_data[i]["HasDelta"].as_bool().unwrap()
+      });
+    }
+
+    self.instructions = InstructionArray;
   }
 
   /**
@@ -116,18 +174,18 @@ impl Downloader {
    * Else download full file.
   */
   pub fn update(&mut self) {
-    if self.instructions_json.is_none() {
+    if self.instructions.len() == 0 {
       self.get_instructions();
     }
-    let _instructions_json = self.instructions_json.clone().unwrap();
     let _release_json = self.release_json.clone().unwrap();
     let mirror = format!("{}{}/", &_release_json["game"]["mirrors"][3]["url"].as_str().unwrap(), &_release_json["game"]["patch_path"].as_str().unwrap());
     DirBuilder::new().recursive(true).create(format!("{}/patcher/",&self.RenegadeX_location.clone().unwrap())).unwrap();
 
-    for i in 0.._instructions_json.len() {
+    for i in 0..self.instructions.len() {
+      println!("{}/{}", i, self.instructions.len());
       //Let's check NewHash if it is supposed to be Null, if it is then the file needs to be deleted.
-      if _instructions_json[i]["NewHash"].is_null() {
-        let path = format!("{}{}", self.RenegadeX_location.clone().unwrap(), _instructions_json[i]["Path"].as_str().unwrap().replace("\\","/"));
+      if self.instructions[i].new_hash.is_none() {
+        let path = format!("{}{}", self.RenegadeX_location.borrow(), self.instructions[i].path.replace("\\","/"));
         match std::fs::remove_file(&path) {
           Ok(()) => (),
           Err(e) => println!("Couldn't remove file: {:?}", e) 
@@ -135,32 +193,47 @@ impl Downloader {
         continue;
       }
       //Compare the installed/existing files with the OldHash 
-      let path = format!("{}{}", self.RenegadeX_location.clone().unwrap(), _instructions_json[i]["Path"].as_str().unwrap().replace("\\","/"));
+      let path = format!("{}{}", self.RenegadeX_location.borrow(), self.instructions[i].path.replace("\\","/"));
       let mut file = match File::open(&path) {
         Ok(file) => file,
         Err(e) => { 
           //Download full file
-          self.download_file(mirror.clone(), _instructions_json[i].clone(), false);
+          for retry in 0..3 {
+            match self.download_file(mirror.clone(), self.instructions[i].clone(), false) {
+              Ok(()) => break,
+              Err(e) => if retry == 2 { panic!("{}", e) }
+            };
+          }
           continue;
         }
       };
       let mut sha256 = Sha256::new();
       io::copy(&mut file, &mut sha256).unwrap();
       let hash = sha256.result();
-      //check if OldHash is not Null (new file), check if the file can be updated otherwise.
-      if _instructions_json[i]["OldHash"].is_null() == false && (&hash[..] == &hex::decode(_instructions_json[i]["OldHash"].as_str().unwrap()).unwrap()[..]) {
+      //check if OldHash is some (not a new file), check if the file can be updated otherwise.
+      if self.instructions[i].old_hash.is_some() && (&hash[..] == &hex::decode(self.instructions[i].old_hash.borrow()).unwrap()[..]) {
         //The installed file's hash is the same as the previous patch's hash
-        if _instructions_json[i]["OldHash"].as_str().unwrap() != _instructions_json[i]["NewHash"].as_str().unwrap() {
+        if self.instructions[i].old_hash.borrow() != self.instructions[i].new_hash.borrow() {
           //a delta should be available, but let's make sure
-          if _instructions_json[i]["HasDelta"].as_bool().unwrap() {
-            self.download_file(mirror.clone(), _instructions_json[i].clone(), true);
+          if self.instructions[i].has_delta {
+            for retry in 0..3 {
+              match self.download_file(mirror.clone(), self.instructions[i].clone(), true) {
+                Ok(()) => break,
+                Err(e) => if retry == 2 { panic!("{}", e) }
+              };
+            }
           }
         }
       } else {
         //Old hash does not match the current file
-        if &hash[..] != &hex::decode(_instructions_json[i]["NewHash"].as_str().unwrap()).unwrap()[..] {
+        if &hash[..] != &hex::decode(self.instructions[i].new_hash.borrow()).unwrap()[..] {
           //Nor does it match the NewHash, thus a full file download is required.
-          self.download_file(mirror.clone(), _instructions_json[i].clone(), false);
+          for retry in 0..3 {
+            match self.download_file(mirror.clone(), self.instructions[i].clone(), false) {
+              Ok(()) => break,
+              Err(e) => if retry == 2 { panic!("{}", e) }
+            };
+          }
         }
       }
     }
@@ -170,13 +243,13 @@ impl Downloader {
   /**
   Downloads a file based on an entry from instructions.json, delta specifies if it has to be the delta or the full file.
   */
-  fn download_file(&self, mirror: String, file: json::JsonValue, delta: bool) {
+  fn download_file(&self, mirror: String, instruction: Instruction, delta: bool) -> Result<(), &'static str> {
     let part_size :usize = 10u64.pow(6) as usize; //1.000.000
     //create a file in download location.
-    let file_path = format!("{}/patcher/{}", &self.RenegadeX_location.clone().unwrap(), &file["NewHash"].as_str().unwrap());
+    let file_path = format!("{}/patcher/{}", &self.RenegadeX_location.clone().unwrap(), instruction.new_hash.borrow());
     let mut f = OpenOptions::new().read(true).write(true).create(true).open(&file_path).unwrap();
     //set the size of the file, add a 32bit integer to the end of the file as a means of tracking progress. We won't download parts async.
-    let finished_file_size : usize = file[if delta {"DeltaSize"} else {"FullReplaceSize"}].as_usize().unwrap();
+    let finished_file_size : usize = if delta { instruction.delta_size } else { instruction.full_replace_size };
     let parts_amount : usize = finished_file_size / part_size + if finished_file_size % part_size > 0 {1} else {0};
     let file_size : usize = finished_file_size + 4;
     if (f.metadata().unwrap().len() as usize) < file_size {
@@ -186,20 +259,23 @@ impl Downloader {
         let mut sha256 = Sha256::new();
         io::copy(&mut f, &mut sha256).unwrap();
         let hash = sha256.result();
-        if &hash[..] == &hex::decode(file[if delta {"DeltaHash"} else {"CompressedHash"}].as_str().unwrap()).unwrap()[..] {
-          return;
+        if &hash[..] == &hex::decode(if delta { instruction.delta_hash.borrow() } else { instruction.compressed_hash.borrow() }).unwrap()[..] {
+          return Ok(());
         }
       }
-      println!("File size ({}) of patch file {} is smaller than it should be ({})",f.metadata().unwrap().len(),file["NewHash"].as_str().unwrap(), file_size);
+      println!("File size ({}) of patch file {} is smaller than it should be ({})",f.metadata().unwrap().len(), instruction.new_hash.borrow(), file_size);
       match f.set_len(file_size as u64) {
         Ok(()) => println!("Succesfully set file size"),
-        Err(e) => println!("Couldn't set file size! {}", e)
+        Err(e) => {
+          println!("Couldn't set file size! {}", e);
+          return Err("Could not change file size of patch file, is it in use?");
+        }
       }
     }
     let download_url = if delta {
-                         format!("{}delta/{}_from_{}", &mirror, &file["NewHash"].as_str().unwrap(), &file["OldHash"].as_str().unwrap())
+                         format!("{}delta/{}_from_{}", &mirror, instruction.new_hash.borrow(), instruction.old_hash.borrow())
                        } else {
-                         format!("{}full/{}", &mirror, &file["NewHash"].as_str().unwrap())
+                         format!("{}full/{}", &mirror, instruction.new_hash.borrow())
                        };
     let http_client = reqwest::Client::new();
     f.seek(SeekFrom::Start((finished_file_size) as u64));
@@ -225,10 +301,21 @@ impl Downloader {
       f.write_all(&(part_int as u32).to_be_bytes());
     }
     println!("Downloaded all parts!");
-    //finally remove the counter at the end of the file
+    //Remove the counter at the end of the file to finish the vcdiff file
     f.set_len(finished_file_size as u64);
-    println!("Shrinked the file!")
-
+    println!("Shrinked the file!");
+    
+    //Let's make sure the downloaded file matches the Hash found in Instructions.json
+    f.seek(SeekFrom::Start(0));
+    let mut sha256 = Sha256::new();
+    io::copy(&mut f, &mut sha256).unwrap();
+    let hash = sha256.result();
+    if &hash[..] != &hex::decode(if delta { instruction.delta_hash.borrow() } else { instruction.compressed_hash.borrow() }).unwrap()[..] {
+      println!("Hash is incorrect!");
+      return Err("Downloaded file's hash did not match with the one provided in Instructions.json");
+      //somehow restart the download :(
+    }
+    return Ok(());
     //created a vcdiff library which is able to decompress this.
   }
 }
@@ -246,26 +333,4 @@ mod tests {
     patcher.update();
     assert!(false);
   }
-/*
-  #[test]
-  fn file_integrity() {
-    let mut patcher : Downloader = Downloader::new();
-    let update : bool = patcher.update_available();
-    assert_eq!(update,true);
-    patcher.RenegadeX_location = Some("/home/sonny/RenegadeX/game_files/".to_string());
-    patcher.check_integrity();
-    assert!(false);
-  }
-*/
-/*
-  #[test]
-  fn sha() {
-    let mut file = File::open("/home/sonny/git/CNC-Walls-correct.udk").unwrap();
-    println!("opening file.");
-    let mut sha256 = Sha256::new();
-    io::copy(&mut file, &mut sha256).unwrap();
-    let hash = sha256.result();
-    println!("hash is: {:x}", hash);
-  }
-*/
 }
