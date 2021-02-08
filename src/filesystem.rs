d86513170989f601f41926d7ac1964f5663bac13
SonnyX <randy@vonderweide.nl>
2021-02-08 15:06:25 +0100

    Modify FileSystem

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,26 +1,43 @@
 use std::ffi::OsString;
 use std::collections::BTreeMap;
-use std::collections::VecDeque;
-use std::sync::Arc;
-use tokio::sync::mpsc::{channel, Sender, Receiver};
+use tokio::sync::mpsc::Receiver;
+use std::collections::btree_map::Entry;
+use crossbeam_queue::SegQueue;
+use crate::futures::Stream;
 
 struct Chunk {
   /// The file the chunk belongs to
   file: OsString,
   /// The part containing the data and the starting position of this data.
   part: Part,
-  /// Is this chunk the last chunk of this file?
-  is_last: bool
 }
+
 struct Part {
   start_location: usize,
   data: Vec<u8>,
 }
 
+struct File {
+  size: usize,
+  done: bool,
+  queue: SegQueue<Part>,
+}
+
+impl File {
+  fn new(size: usize) -> Self {
+    File {
+      size,
+      done: false,
+      queue: SegQueue::new()
+    }
+  }
+}
+
 // Set up a singular task which takes care of writing data to disk
 struct FileSystem {
-  parts: BTreeMap<OsString, VecDeque<Part>>,
+  parts: BTreeMap<OsString, File>,
   use_memory_only: bool,
+  receiver: Receiver<Chunk>,
 }
 
 
@@ -28,19 +45,31 @@ struct FileSystem {
 // Have a loop somewhere that is responsible for receiving parts from elsewhere, add them to a hashmap.
 // Have a loop somewhere that is responsible for writing parts of which the most is available, however, we don't want to run all the time.
 impl FileSystem {
-  pub async fn receive_parts(&mut self, mut receiver: Receiver<Chunk>) {
-    while let Some(chunk) = receiver.recv().await {
-      self.parts.entry(chunk.file).or_insert(VecDeque::new()).push_back(chunk.part);
+  pub async fn receive_parts(&mut self) {
+    while let Some(chunk) = self.receiver.recv().await {
+      match self.parts.get(&chunk.file) {
+        Some(file) => {
+          file.queue.push(chunk.part);
+          
+        },
+        None => {
+          panic!("This shouldn't happen");
+        }
+      }
     }
   }
 
   async fn get_most_backed_up(&self) -> Option<OsString> {
     self.parts.iter()
-    .max_by(|a, b| a.1.len().cmp(&b.1.len()))
+    .max_by(|a, b| a.1.queue.len().cmp(&b.1.queue.len()))
     .map(|(k, _v)| k.clone())
   }
 
   pub async fn write_parts_to_disk(&self) {
-    
+    // self.receiver.
+    // while let Some(most_backed_up) = self.get_most_backed_up().await {
+    //   let receiver = self.receivers.get(most_backed_up);
+
+    // }
   }
 }
