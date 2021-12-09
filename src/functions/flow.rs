8fbfeb71ad3b8eaa11211c1790f7257f1843c838
SonnyX <randy@vonderweide.nl>
2021-12-09 22:02:24 +0100

    logging attempt

--- src/functions/flow.rs
+++ src/functions/flow.rs
@@ -12,28 +12,28 @@ pub async fn flow(mut mirrors: Mirrors, game_location: String, instructions_hash
   progress.set_current_action("Testing mirrors!".to_string())?;
   progress_callback(&progress);
   mirrors.test_mirrors().await?;
-
+  
   progress.set_current_action("Downloading instructions file!".to_string())?;
   progress_callback(&progress);
-
+  
   // Download Instructions.json
   let instructions = retrieve_instructions(instructions_hash, &mirrors).pausable().await?;
   
   progress.set_current_action("Parsing instructions file!".to_string())?;
   progress_callback(&progress);
-
+  
   // Parse Instructions.json
   let mut instructions = parse_instructions(instructions)?;
   instructions.sort_by(|a, b| a.full_vcdiff_size.cmp(&b.full_vcdiff_size));
-
+  
   progress.set_current_action("Processing instructions!".to_string())?;
   progress_callback(&progress);
-
+  
   let mut futures : Box<FuturesUnordered<_>> = Box::new(FuturesUnordered::new());
   progress.set_instructions_amount(instructions.len().try_into().expect("Somehow we have more than 2^64 instructions, colour me impressed"));
   let handle = tokio::runtime::Handle::current();
-
-
+  
+  
   for instruction in instructions {
     let mirrors = mirrors.clone();
     let progress = progress.clone();
@@ -41,8 +41,8 @@ pub async fn flow(mut mirrors: Mirrors, game_location: String, instructions_hash
   }
   progress.set_current_action("Validating, Downloading, Patching!".to_string())?;
   progress_callback(&progress);
-
-
+  
+  
   let (future, abort_handle) = futures::future::abortable(async move {
     loop {
       tokio::time::sleep(Duration::from_millis(250)).await;
@@ -77,33 +77,34 @@ pub async fn flow(mut mirrors: Mirrors, game_location: String, instructions_hash
   abort_handle.abort();
   //let futures = futures::future::try_join_all(futures).await;
   //let progress_update = futures::future::abortable(progress.call_every(Duration::from_millis(250)));
-
+  
   //futures::future::select(futures, progress_update.0).await;
   Ok(())
 }
 
 async fn process_instruction(instruction: Instruction, mirrors: Mirrors, progress: Progress) -> Result<Instruction, Error> {
   let action = instruction.determine_action().await?;
-      progress.increment_processed_instructions();
+  println!("Determined action {:?} for {}", &action, &instruction.path);
+  progress.increment_processed_instructions();
+  
+  match action {
+    crate::structures::Action::DownloadFull => {
+      let file = instruction.newest_hash.clone().expect("Download full, but there's no full vcdiff hash");
+      download_file_in_parallel("full", file, instruction.full_vcdiff_size, mirrors, progress).await?;
       
-      match action {
-        crate::structures::Action::DownloadFull => {
-          let file = instruction.newest_hash.clone().expect("Download full, but there's no full vcdiff hash");
-          download_file_in_parallel("full", file, instruction.full_vcdiff_size, mirrors, progress).await?;
-
-          //apply_patch(instruction.path, instruction.full_vcdiff_hash, instruction.full_vcdiff_hash, false);
-          //progress.increment_patched_done();
-          Ok(instruction)
-        },
-        crate::structures::Action::DownloadDelta => {
-          let file = format!("{}_from_{}", &instruction.newest_hash.clone().expect("Download delta, but there's no newest hash"), &instruction.previous_hash.clone().expect("Download delta, but there's no previous hash"));
-          download_file_in_parallel("delta", file, instruction.delta_vcdiff_size, mirrors, progress).await?;
-
-          //apply_patch(instruction.path, instruction.full_vcdiff_hash, instruction.full_vcdiff_hash, true);
-          Ok(instruction)
-        },
-        crate::structures::Action::Nothing => {
-          Ok(instruction)
-        },
-      }
+      //apply_patch(instruction.path, instruction.full_vcdiff_hash, instruction.full_vcdiff_hash, false);
+      //progress.increment_patched_done();
+      Ok(instruction)
+    },
+    crate::structures::Action::DownloadDelta => {
+      let file = format!("{}_from_{}", &instruction.newest_hash.clone().expect("Download delta, but there's no newest hash"), &instruction.previous_hash.clone().expect("Download delta, but there's no previous hash"));
+      download_file_in_parallel("delta", file, instruction.delta_vcdiff_size, mirrors, progress).await?;
+      
+      //apply_patch(instruction.path, instruction.full_vcdiff_hash, instruction.full_vcdiff_hash, true);
+      Ok(instruction)
+    },
+    crate::structures::Action::Nothing => {
+      Ok(instruction)
+    },
+  }
 }
--- src/implementations/instruction.rs
+++ src/implementations/instruction.rs
@@ -1,66 +1,66 @@
 use std::path::Path;
 
-use crate::functions::{get_hash, delete_file, restore_backup};
+use crate::functions::{delete_file, get_hash, restore_backup};
 use crate::structures::{Action, Error, Instruction};
 
 impl Instruction {
-    pub async fn determine_action(self: &Instruction) -> Result<Action, Error> {
-        let backup_path = format!("{}.bck", &self.path);
-        let mut backup_hash = None;
-        let path_exists = Path::new(&self.path).exists();
-        let backup_exists = Path::new(&backup_path).exists();
-        // Determine wether we have to delete files, update them, or add them.
-        if let Some(newest_hash) = self.newest_hash.clone() {
-            let mut hash = None;
-            // Update or download
-            if path_exists {
-                hash = Some(get_hash(&self.path).await?);
-                if newest_hash.eq(&hash.clone().unwrap()) {
-                    // File is already newest file
-                    if backup_exists {
-                        delete_file(&backup_path).await;
-                    }
-                    return Ok(Action::Nothing);
-                }
-            }
-    
-            if backup_exists {
-                backup_hash = Some(get_hash(&backup_path).await?);
-                if backup_hash.clone().map(|backup_hash| newest_hash.eq(&backup_hash)).unwrap() {
-                    // Restore backup file
-                    restore_backup(&self.path).await;
-                    return Ok(Action::Nothing);
-                }
-            }
-    
-            // File is not up to date
-            if let Some(previous_hash ) = self.previous_hash.clone() {
-                if path_exists && previous_hash.eq(&hash.clone().unwrap()) {
-                    // Download delta
-                    return Ok(Action::DownloadDelta);
-                } else {
-                    // Check if there's a backup file, and restore it if it matches previous_hash
-                    if backup_exists && previous_hash.eq(&backup_hash.clone().unwrap()) {
-                        // Restore backup file
-                        restore_backup(&self.path).await;
-                        return Ok(Action::DownloadDelta);
-                    }
-                    // Download full
-                    return Ok(Action::DownloadFull);
-                }
-            }       
-    
-            // Download full
-            return Ok(Action::DownloadFull);
-        } else {
-            // Delete file
-            if backup_exists {
-                delete_file(&backup_path).await;
-            }
-            if path_exists {
-                delete_file(&self.path).await;
-            }
+  pub async fn determine_action(self: &Instruction) -> Result<Action, Error> {
+    let backup_path = format!("{}.bck", &self.path);
+    let mut backup_hash = None;
+    let path_exists = Path::new(&self.path).exists();
+    let backup_exists = Path::new(&backup_path).exists();
+    // Determine wether we have to delete files, update them, or add them.
+    if let Some(newest_hash) = self.newest_hash.clone() {
+      let mut hash = None;
+      // Update or download
+      if path_exists {
+        hash = Some(get_hash(&self.path).await?);
+        if newest_hash.eq(&hash.clone().unwrap()) {
+          // File is already newest file
+          if backup_exists {
+            delete_file(&backup_path).await;
+          }
+          return Ok(Action::Nothing);
         }
-        Ok(Action::Nothing)
+      }
+      
+      if backup_exists {
+        backup_hash = Some(get_hash(&backup_path).await?);
+        if backup_hash.clone().map(|backup_hash| newest_hash.eq(&backup_hash)).unwrap() {
+          // Restore backup file
+          restore_backup(&self.path).await;
+          return Ok(Action::Nothing);
+        }
+      }
+      
+      // File is not up to date
+      if let Some(previous_hash) = self.previous_hash.clone() {
+        if path_exists && previous_hash.eq(&hash.clone().unwrap()) {
+          // Download delta
+          return Ok(Action::DownloadDelta);
+        } else {
+          // Check if there's a backup file, and restore it if it matches previous_hash
+          if backup_exists && previous_hash.eq(&backup_hash.clone().unwrap()) {
+            // Restore backup file
+            restore_backup(&self.path).await;
+            return Ok(Action::DownloadDelta);
+          }
+          // Download full
+          return Ok(Action::DownloadFull);
+        }
+      }
+      
+      // Download full
+      return Ok(Action::DownloadFull);
+    } else {
+      // Delete file
+      if backup_exists {
+        delete_file(&backup_path).await;
+      }
+      if path_exists {
+        delete_file(&self.path).await;
+      }
     }
+    Ok(Action::Nothing)
+  }
 }
